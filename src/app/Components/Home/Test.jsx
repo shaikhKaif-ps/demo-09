5a839a055021c072600a59beea398afbf194ed08
sk-kaif <kaif.s@parashifttech.com>
2025-01-09 12:50:53 +0530

    first commit

---
+++ src/app/Components/Home/Test.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Test = async() => {
+    const result = await leadershipAPI();
+    
+  return (
+    <div>
+        {
+            result &&
+            result.map((item)=>{
+                return <h1 className='my-5 bg-cyan-400' key={item.id}>{item?.acf.leadership_designation} </h1>
+            })
+        }
+    </div>
+  )
+}
+
+export default Test
+
+
+
+
+async function leadershipAPI() {
+    const response = await fetch('https://wordpress-1034502-4717804.cloudwaysapps.com/wp-json/wp/v2/leadership_team', {
+        next:{revalidate:10}
+      });
+
+    if (!response.ok) {
+        throw new Error('An error occurred while fetching the data');
+    }
+    const posts = await response.json();
+
+  return (
+    posts
+  )
+}
---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Advantage from "./Components/Home/Advantage";
+import SpotLight from "./Components/Home/SpotLight";
+import Test from "./Components/Home/Test";
+
+export default function Home() {
+  return (
+   <>
+    {/* <SpotLight /> */}
+    {/* <Advantage /> */}
+    <Test />
+   </>
+  );
+}
