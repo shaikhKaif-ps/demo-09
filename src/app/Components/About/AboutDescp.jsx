d243df3a5b5b7f343bfe71e51c09c2810a233604
sk-kaif <kaif.s@parashifttech.com>
2025-01-10 19:51:36 +0530

    tclFull

--- src/app/Components/About/AboutDescp.jsx
+++ src/app/Components/About/AboutDescp.jsx
@@ -1,24 +1,31 @@
-
 const AboutDescp = () => {
   return (
     <main className="ourContainer pb-14">
-        <h2 className="text-5xl font-semibold pb-1">About Us</h2>
-                <h3 className="text-2xl pb-4">We drive on the passion for excellence.</h3>
-        <div className="md:flex justify-between gap-10">
-            <div className="leftCon  md:w-[40%] flex flex-col gap-4 text-base">
-                <p>Since 2001, our customers have experienced only the finest logistics services. The unshakeable trust of our customers comes from the promises we have delivered every time. As a result, we have established ourselves among the premier services providers in India's Clearing and Forwarding industry for over two decades</p>
-            </div>
-            <div className="rightCon  md:w-[60%] flex flex-col gap-4 text-base">
-                <p>From a humble start, Capricorn Logistics has swiftly spread its network globally. Currently, we have our presence across 32 Indian and ten overseas offices in major economies like the United States of America, Europe, China, Africa, and the United Arab Emirates.</p>
-                <p>Our vision has always been to deliver superior logistics service quality. Capricorn achieves the ultimate customer satisfaction by coupling human expertise with logistics technology.</p>
-                <p>Over the years, our expertise has grown with us. Today, we execute projects including Air Freight, Ocean Freight, Surface Transportation, Door To Door Express Service, Customs Brokerage, 3PL, 4PL & 5PL, and Industrial Solutions.</p>
-                <p>Capricorn Logistics aspires to give customers the maximum output of their investment of time, trust, and costs. Our forte is to deeply understand the requirements and design logistics and freight solutions to help businesses gain a competitive advantage. No matter what your supply chain challenges are, Capricorn is adept at delivering value for businesses of all sizes.</p>
-            </div>
+      <h2 className="text-5xl font-semibold pb-1">About Us</h2>
+      <h3 className="text-2xl pb-4">We drive on the passion for excellence.</h3>
+      <div className="md:flex justify-between gap-10">
+        <div className="leftCon  md:w-[40%] flex flex-col gap-4 text-base">
+          <p>
+            Since 2001, our customers have experienced only the finest logistics services. The unshakeable trust of our customers comes from the promises we have delivered every time. As a result, we have established ourselves among the premier services providers in India&#39;s Clearing and Forwarding industry for over two decades.
+          </p>
         </div>
-
-        
+        <div className="rightCon  md:w-[60%] flex flex-col gap-4 text-base">
+          <p>
+            From a humble start, Capricorn Logistics has swiftly spread its network globally. Currently, we have our presence across 32 Indian and ten overseas offices in major economies like the United States of America, Europe, China, Africa, and the United Arab Emirates.
+          </p>
+          <p>
+            Our vision has always been to deliver superior logistics service quality. Capricorn achieves the ultimate customer satisfaction by coupling human expertise with logistics technology.
+          </p>
+          <p>
+            Over the years, our expertise has grown with us. Today, we execute projects including Air Freight, Ocean Freight, Surface Transportation, Door To Door Express Service, Customs Brokerage, 3PL, 4PL & 5PL, and Industrial Solutions.
+          </p>
+          <p>
+            Capricorn Logistics aspires to give customers the maximum output of their investment of time, trust, and costs. Our forte is to deeply understand the requirements and design logistics and freight solutions to help businesses gain a competitive advantage. No matter what your supply chain challenges are, Capricorn is adept at delivering value for businesses of all sizes.
+          </p>
+        </div>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default AboutDescp
+export default AboutDescp;
